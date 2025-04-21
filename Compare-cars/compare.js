335f017c554b0675f8cd172b263853a2521dd0c4
bishoDev123 <bisholiveenglish@gmail.com>
2025-04-21 17:21:34 +0200

    did the initial setup to begin working right away

--- Compare-cars/compare.js
+++ Compare-cars/compare.js
@@ -4,7 +4,16 @@ fetch('../cars.JSON')
         const test = document.getElementById("test");
         const car = cars.cars[0];
 
-        test.innerHTML = `<h1>${car.name}</h1>`
+        const formattedPrice = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        }).format(car.price);
+
+        test.innerHTML = 
+            `<img src="${car.image}"></img>
+             <h1>${car.name}</h1>
+             <h2>${formattedPrice}</h2>
+             <p>${car.description}</p>`
     }).catch(error => {
         console.error('Error loading JSON:', error);
       });

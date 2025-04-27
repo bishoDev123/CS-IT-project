3864f4d696f41ce088e21d0c52575b8f80a17040
bishoDev123 <bisholiveenglish@gmail.com>
2025-04-27 14:32:23 +0300

    added the initial select screen html and css with json integration included

--- Compare-cars/compare.js
+++ Compare-cars/compare.js
@@ -1,19 +1,34 @@
-fetch('../cars.JSON')
-    .then(response => response.json())
-    .then(cars => {
-        const test = document.getElementById("test");
-        const car = cars.cars[0];
+fetch("../cars.JSON")
+  .then((response) => response.json())
+  .then((data) => {
+    const selects = document.querySelectorAll("#selectedCar");
+    const cars = data.cars;
 
-        const formattedPrice = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        }).format(car.price);
-
-        test.innerHTML = 
-            `<img src="${car.image}"></img>
-             <h1>${car.name}</h1>
-             <h2>${formattedPrice}</h2>
-             <p>${car.description}</p>`
-    }).catch(error => {
-        console.error('Error loading JSON:', error);
-      });
+    const optionsHTML =
+      `
+            <option value="" disabled selected>Select a car</option>
+        ` +
+      cars
+        .map(
+          (car) => `
+            <option value="${car.name}">${car.name} - ${new Intl.NumberFormat(
+            "en-US",
+            {
+              style: "currency",
+              currency: "USD",
+            }
+          ).format(car.price)}</option>
+        `
+        )
+        .join("");
+    selects.forEach((select) => {
+        select.innerHTML = optionsHTML;
+    });
+  })
+  .catch((error) => {
+    //console fall safe display
+    console.error("Error loading JSON:", error);
+    //visual display
+    document.body.innerHTML +=
+      "<p>Failed to load car data. Please try again later.</p>";
+  });

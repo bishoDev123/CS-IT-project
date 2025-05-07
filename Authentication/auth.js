5ee00bdd403f36631aa0fe72ad2a026624794644
Ahmed-Elkammar <ahmadfwz1412@gmail.com>
2025-05-07 21:59:33 +0300

    j

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -19,15 +19,16 @@ form.addEventListener("submit", function (e) {
         alert("please input valied E-mail.");
         return;
     }
-
+    const password = document.getElementById("password").value;
+    if (password.length < 8) {
+        alert("password can't be less than 8 characters.")
+        return;
+    }
     const registerPage = form.querySelector("h1").textContent.toLowerCase().includes("register");
     if (registerPage) {
-        const password = document.getElementById("password").value;
         const confirm = document.getElementById("confirm").value;
-        if (password.length < 8) {
-            alert("password can't be less than 8 characters.")
-            return;
-        }
+
+
         if (password !== confirm) {
             alert("password and Confirm password don't match.")
             return;
